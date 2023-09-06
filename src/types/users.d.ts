c81f28bc5a8eee43411c439279c01d93a73f5878
WasathTheekshana <wasath.vt@gmail.com>
2023-09-06 14:14:08 +0530

    fix: new user type fix

--- src/types/users.d.ts
+++ src/types/users.d.ts
@@ -1,19 +1,8 @@
 export interface IRegisterUser {
-  userRole: string;
   fullName: string;
   email: string;
   universityMail: string;
-  nic: string;
-  profileImgUrl: string;
   contactNumber: number;
-  whatsAppNumber: number;
-  foodPreference: string;
-  isNsbmStudent: boolean;
-  universityName?: string;
-  studentId?: number;
-  batch?: string;
-  isAttended: boolean;
-}
-export interface IUpdateUser {
-  profileImgUrl: string;
+  studentId: number;
+  batch: string;
 }
