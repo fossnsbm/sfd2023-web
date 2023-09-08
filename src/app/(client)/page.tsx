178ae8ca40103473f46a49755b59ba7d409f7b18
WasathTheekshana <wasath.vt@gmail.com>
2023-09-08 12:02:25 +0530

    style: margin n padding on stat section

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,10 +1,14 @@
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Stats from "@/components/Stats";
+import Speakers from "@/components/speakers/Speakers";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Stats />
+      <Speakers />
       <Footer />
     </>
   );
