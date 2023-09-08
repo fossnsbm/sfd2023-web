31dfb0eb1db9f329614c70a621c636f5b7ba30a2
Hasini Punsala <hasinipunsala@gmail.com>
2023-09-08 11:47:04 +0530

    feat: stats section

---
+++ src/data/stats.ts
@@ -0,0 +1,22 @@
+export const stats = [
+    {
+        id: 1,
+        name: 'Attendees',
+        value: '300+',
+    },
+    {
+        id: 1,
+        name: 'Communities',
+        value: '6+',
+    },
+    {
+        id: 1,
+        name: 'Speakers',
+        value: '5+',
+    },
+    {
+        id: 1,
+        name: 'Sessions',
+        value: '3+',
+    },
+];
