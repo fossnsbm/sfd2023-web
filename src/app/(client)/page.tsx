50aec4f0e5221dbaee116ad996a9ce2916e935bb
yasithranusha <yasithranusha24@gmail.com>
2023-09-08 21:17:18 +0530

    Add event-outline component

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Intro from "@/components/Intro";
 import Navbar from "@/components/Navbar";
 import Stats from "@/components/Stats";
+import EventOutline from "@/components/event-outline/EventOutline";
 import Communities from "@/components/communities/Communities";
 import Speakers from "@/components/speakers/Speakers";
 
@@ -13,6 +14,7 @@ export default function Home() {
       <Hero />
       <Intro />
       <Stats />
+      <EventOutline />
       <Speakers />
       <Communities />
       <Footer />
