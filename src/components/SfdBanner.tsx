24dfdbeeb04bc9d3dca6290deac1c69a798a32e1
Hasini Punsala <hasinipunsala@gmail.com>
2023-09-09 12:15:24 +0530

    style: animate sfd banner

--- src/components/SfdBanner.tsx
+++ src/components/SfdBanner.tsx
@@ -1,17 +1,28 @@
+'use client'
+
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 
 type Props = {}
 
 const SfdBanner = (props: Props) => {
     return (
-        <div className='relative h-[200px] md:h-[350px]'>
-            <Image
-                src='/images/sfd-3d.png'
-                fill
-                style={{ objectFit: 'cover' }}
-                className='rounded-2xl'
-                alt='sfd banner'
-            />
+        <div className='relative h-[200px] md:h-[350px] overflow-hidden rounded-2xl'>
+            <motion.div
+                className='relative h-[200px] md:h-[350px]'                 
+                initial={{scale: 1.1}} 
+                whileInView={{scale: 1}}
+                whileHover={{scale: 1.1}}    
+                transition={{duration: 0.5, delay: 0.1, ease: 'easeInOut'}}
+            >
+                <Image
+                    src='/images/sfd-3d.png'
+                    layout='fill'
+                    style={{ objectFit: 'cover' }}
+                    className='rounded-2xl'
+                    alt='sfd banner'
+                />
+            </motion.div>
         </div>
     )
 }
